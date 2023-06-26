a110b7a93b148bcd7426242e9ba804154e2b21b1
Puk06 <86549420+puk06@users.noreply.github.com>
2023-06-26 13:48:09 +0900

    Update ODscaled.js

--- OD/ODscaled.js
+++ OD/ODscaled.js
@@ -1,10 +1,10 @@
 module.exports.ODscaled = (od, mods) => {
 	if (mods.includes("EZ")) {
-		od /= 2;
-	};
+		od /= 2
+	}
 	if (mods.includes("HR")) {
-		od *= 1.4;
-	};
+		od *= 1.4
+	}
 	od = Math.max(Math.min(od, 10), 0);
 	return od;
-};
+}
