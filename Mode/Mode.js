67ff26a9fb493420963aed3175143702d1ca83ad
Puk06 <86549420+puk06@users.noreply.github.com>
2023-07-11 11:42:52 +0900

    Add files via upload

--- Mode/Mode.js
+++ Mode/Mode.js
@@ -1,29 +1,43 @@
 module.exports.modeconvert = (mode) => {
-    if(mode == "osu"){
+    if (mode == "osu") {
         return "0"
-    }else if(mode == "taiko"){
+    } else if(mode == "taiko") {
         return "1"
-    }else if(mode == "fruits"){
+    } else if(mode == "fruits") {
         return "2"
-    }else if(mode == "mania"){
+    } else if(mode == "mania") {
         return "3"
-    }else if(mode == "0"){
+    } else if(mode == "0") {
         return "osu"
-    }else if(mode == "1"){
+    } else if(mode == "1") {
         return "taiko"
-    }else if(mode == "2"){
-        return "fruits"
-    }else if(mode == "3"){
+    } else if(mode == "2") {
+        return "catch"
+    } else if(mode == "3") {
         return "mania"
-    }else if(mode == "o"){
+    } else if(mode == "o") {
         return "0"
-    }else if(mode == "t"){
+    } else if(mode == "t") {
         return "1"
-    }else if(mode == "c"){
+    } else if(mode == "c") {
         return "2"
-    }else if(mode == "m"){
+    } else if(mode == "m") {
         return "3"
-    }else{
+    } else {
         return "nomode"
-    };
-};
+    }
+}
+
+module.exports.modeconvertforlinks = (mode) => {
+    if (mode == "0") {
+        return "osu"
+    } else if (mode == "1") {
+        return "taiko"
+    } else if (mode == "2") {
+        return "fruits"
+    } else if (mode == "3") {
+        return "mania"
+    } else {
+        return "nomode"
+    }
+}
