6aa3d9215c15b642fe5623c08ab71888c9725812
Puk06 <86549420+puk06@users.noreply.github.com>
2023-08-17 13:24:16 +0900

    UPdatedd

--- HoshinoBot.js
+++ HoshinoBot.js
@@ -4523,11 +4523,11 @@ client.on(Events.MessageCreate, async (message) =>
 		}
 
 		//時間計算機
-		if (/^[0-9.]+時間?$/.test(message.content) && message.content.includes(".")) {
+		if (/^[0-9.]+時間?$/.test(message.content) && message.content.includes(".") && !/^\.+$/.test(message.content)) {
 			const totalHours = parseFloat(message.content.split("時間")[0])
 			if (!message.content.split("時間")[0].includes(".")) return
 			message.reply(`${Math.floor(totalHours)}時間 ${Math.floor((totalHours - Math.floor(totalHours)) * 60)}分 ${Math.round(((totalHours - Math.floor(totalHours)) * 60 - Math.floor((totalHours - Math.floor(totalHours)) * 60)) * 60)}秒`);
-		} else if (/^[0-9.]+分?$/.test(message.content) && message.content.includes(".")) {
+		} else if (/^[0-9.]+分?$/.test(message.content) && message.content.includes(".") && !/^\.+$/.test(message.content)) {
 			const totalminutes = parseFloat(message.content.split("分")[0])
 			if (!message.content.split("分")[0].includes(".")) return
 			message.reply(`${Math.floor(totalminutes)}分 ${Math.round((totalminutes - Math.floor(totalminutes)) * 60)}秒`);
